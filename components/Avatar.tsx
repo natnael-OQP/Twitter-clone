319e33116279cd69cd89181628c84087a8c94f4d
natnael-OQP <makiya112545@gmail.com>
2022-05-19 10:42:54 +0300

    Avatar Component created

---
+++ components/Avatar.tsx
@@ -0,0 +1,23 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+interface Props {
+    image: string
+    size: number
+}
+
+const Avatar = ({ image, size = 50, ...other }: Props) => {
+    return (
+        <Image
+            style={{
+                height: size,
+                width: size,
+                borderRadius: size / 2,
+                ...other,
+            }}
+            source={{ uri: image }}
+        />
+    )
+}
+
+export default Avatar
