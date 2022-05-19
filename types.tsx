d188ed88f8df8ccef60f6b7310b9bd49e39c28cb
natnael-OQP <makiya112545@gmail.com>
2022-05-19 23:47:13 +0300

    add User and Tweets type

--- types.tsx
+++ types.tsx
@@ -3,37 +3,55 @@
  * https://reactnavigation.org/docs/typescript/
  */
 
-import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
 import {
-  CompositeScreenProps,
-  NavigatorScreenParams,
-} from "@react-navigation/native";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
+    CompositeScreenProps,
+    NavigatorScreenParams,
+} from '@react-navigation/native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
 declare global {
-  namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
-  }
+    namespace ReactNavigation {
+        interface RootParamList extends RootStackParamList {}
+    }
 }
 
 export type RootStackParamList = {
-  Root: NavigatorScreenParams<RootTabParamList> | undefined;
-  Modal: undefined;
-  NotFound: undefined;
-};
+    Root: NavigatorScreenParams<RootTabParamList> | undefined
+    Modal: undefined
+    NotFound: undefined
+}
 
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
-  NativeStackScreenProps<RootStackParamList, Screen>;
+    NativeStackScreenProps<RootStackParamList, Screen>
 
 export type RootTabParamList = {
-  Home: undefined;
-  Search: undefined;
-  Notifications: undefined;
-  Messages: undefined;
-};
+    Home: undefined
+    Search: undefined
+    Notifications: undefined
+    Messages: undefined
+}
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> =
-  CompositeScreenProps<
-    BottomTabScreenProps<RootTabParamList, Screen>,
-    NativeStackScreenProps<RootStackParamList>
-  >;
+    CompositeScreenProps<
+        BottomTabScreenProps<RootTabParamList, Screen>,
+        NativeStackScreenProps<RootStackParamList>
+    >
+
+export type UserProps = {
+    id: string
+    username: string
+    name: string
+    image?: string
+}
+
+export type TweetsProps = {
+    id: string
+    user: UserProps
+    createdAt: string
+    content: string
+    image?: string
+    numberOfComments: number
+    numberOfRetweets: number
+    numberOfLikes: number
+}
