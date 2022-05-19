40ad89070a1d482f05a7ea8991a531c2244c299e
natnael-OQP <makiya112545@gmail.com>
2022-05-19 23:48:16 +0300

    create Icon Component

---
+++ components/Icon.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { EvilIcons } from '@expo/vector-icons'
+
+interface Props {
+    name: string
+    size?: number
+    color?: string
+    number?: number
+}
+
+const Icon = ({ name, size = 35, color = '#ccc', number }: Props) => {
+    return (
+        <View style={styles.container}>
+            <EvilIcons name={name} size={size} color={color} />
+            {number && <Text style={styles.iconText}>{number}</Text>}
+        </View>
+    )
+}
+
+export default Icon
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    iconText: {
+        marginLeft: 5,
+        color: '#ccc',
+    },
+})
