fe8c144a4a00cddf123c7e4dd664fc0bf36730ef
natnael-OQP <makiya112545@gmail.com>
2022-05-19 10:44:37 +0300

    replace to Root diractori

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -1,13 +1,21 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { useEffect } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
 
-import { Text, View } from '../components/Themed';
-import { RootStackScreenProps } from '../types';
+import { Text, View } from "../components/Themed";
+import { RootStackScreenProps } from "../types";
 
-export default function NotFoundScreen({ navigation }: RootStackScreenProps<'NotFound'>) {
+export default function NotFoundScreen({
+  navigation,
+}: RootStackScreenProps<"NotFound">) {
+  useEffect(() => navigation.replace("Root"), []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>This screen doesn't exist.</Text>
-      <TouchableOpacity onPress={() => navigation.replace('Root')} style={styles.link}>
+      <TouchableOpacity
+        onPress={() => navigation.replace("Root")}
+        style={styles.link}
+      >
         <Text style={styles.linkText}>Go to home screen!</Text>
       </TouchableOpacity>
     </View>
@@ -17,13 +25,13 @@ export default function NotFoundScreen({ navigation }: RootStackScreenProps<'Not
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   link: {
     marginTop: 15,
@@ -31,6 +39,6 @@ const styles = StyleSheet.create({
   },
   linkText: {
     fontSize: 14,
-    color: '#2e78b7',
+    color: "#2e78b7",
   },
 });
