33e09b9f2fc964e136b0f52a007eb44c2fc27848
natnael-OQP <makiya112545@gmail.com>
2022-05-21 23:03:23 +0300

    create NewTweetScreen

---
+++ screens/NewTweetScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NewTweetScreen = () => {
+    return (
+        <View>
+            <Text>NewTweetScreen</Text>
+        </View>
+    )
+}
+
+export default NewTweetScreen
+
+const styles = StyleSheet.create({})
