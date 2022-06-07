7d04978e73f6195ae69df9f1ac7a7a0e929e0bea
natnael-OQP <makiya112545@gmail.com>
2022-06-07 22:53:51 +0300

    create login ui

--- App.tsx
+++ App.tsx
@@ -8,9 +8,12 @@ import Navigation from './navigation'
 import { Amplify } from 'aws-amplify'
 import awsconfig from './src/aws-exports'
 
+import { withAuthenticator } from 'aws-amplify-react-native'
 Amplify.configure(awsconfig)
 
-export default function App() {
+// import '@aws-amplify/ui-react/styles.css'
+
+function App() {
     const isLoadingComplete = useCachedResources()
     const colorScheme = useColorScheme()
 
@@ -25,3 +28,5 @@ export default function App() {
         )
     }
 }
+
+export default withAuthenticator(App)
