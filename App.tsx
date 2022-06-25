dc46c56739029a27a57839848cf06007a3efe37a
natnael-OQP <makiya112545@gmail.com>
2022-06-25 23:26:23 +0300

    create user

--- App.tsx
+++ App.tsx
@@ -5,12 +5,14 @@ import useCachedResources from './hooks/useCachedResources'
 import useColorScheme from './hooks/useColorScheme'
 import Navigation from './navigation'
 // aws amplify
-import { Amplify } from 'aws-amplify'
+import { Amplify, API, graphqlOperation } from 'aws-amplify'
 import awsconfig from './src/aws-exports'
 
 import { withAuthenticator } from 'aws-amplify-react-native'
 import { useEffect } from 'react'
 import { Auth } from 'aws-amplify'
+import { getUser } from './graphql/queries'
+import { createUser } from './graphql/mutations'
 
 Amplify.configure(awsconfig)
 
@@ -18,10 +20,33 @@ function App() {
     const isLoadingComplete = useCachedResources()
     const colorScheme = useColorScheme()
 
+    const saveUserToDB = async (user: any) => {
+        await API.graphql(graphqlOperation(createUser, { input: user }))
+    }
+
     useEffect(() => {
         const updateUser = async () => {
-            const user = await Auth.currentAuthenticatedUser()
-            console.log(user)
+            const authUser = await Auth.currentAuthenticatedUser()
+
+            if (authUser) {
+                // check if user already exist in database
+                const { data }: any = await API.graphql(
+                    graphqlOperation(getUser, { id: authUser.attributes.sub })
+                )
+
+                if (!data.getUser) {
+                    const user = {
+                        id: authUser.attributes.sub,
+                        username: authUser.username,
+                        name: authUser.username,
+                        email: authUser.attributes.email,
+                        image: 'http://cdn.onlinewebfonts.com/svg/img_215059.png',
+                    }
+                    await saveUserToDB(user)
+                } else {
+                    console.log('user already exists')
+                }
+            }
         }
 
         updateUser()
