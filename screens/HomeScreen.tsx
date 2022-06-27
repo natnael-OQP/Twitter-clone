a10ad5be24164ecd16a3795a310bd2c171032584
natnael-OQP <makiya112545@gmail.com>
2022-06-27 20:33:40 +0300

    fetch tweets

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,39 @@
 import { FlatList, StyleSheet } from 'react-native'
 
-import { View } from '../components/Themed'
+import { Text, View } from '../components/Themed'
 import { RootTabScreenProps } from '../types'
 import TweetComponent from '../components/TweetComponent'
 
-import tweets from '../assets/data/tweets'
 import Separator from '../components/Separator'
 import CreateTweetButton from '../components/CreateTweetButton'
+import { useEffect, useState } from 'react'
+import { API, graphqlOperation } from 'aws-amplify'
+import { listTweets } from '../graphql/queries'
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
+    const [tweets, setTweets] = useState()
+    const [loading, setLoading] = useState(false)
+
+    const fetchTweets = async () => {
+        setLoading(true)
+        try {
+            const { data } = await API.graphql(graphqlOperation(listTweets))
+            setTweets(data.listTweets.items)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchTweets()
+    }, [listTweets])
+
+    if (!tweets) {
+        return <Text>Loading ....</Text>
+    }
+
     return (
         <View style={styles.container}>
             <FlatList
@@ -17,6 +42,8 @@ export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
                 renderItem={({ item }) => <TweetComponent tweet={item} />}
                 showsVerticalScrollIndicator={false}
                 ItemSeparatorComponent={Separator}
+                refreshing={loading}
+                onRefresh={fetchTweets}
             />
             <CreateTweetButton />
         </View>
@@ -25,8 +52,9 @@ export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#F2F2F2',
+        backgroundColor: '#FFF',
         position: 'relative',
+        flex: 1,
     },
     title: {
         fontSize: 20,
