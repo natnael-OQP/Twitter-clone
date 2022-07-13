8f44c0a24b5fe3ef70c84a83fc7c54e863679e84
natnael-OQP <makiya112545@gmail.com>
2022-07-13 22:55:19 +0300

    update backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "twitterclone": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
